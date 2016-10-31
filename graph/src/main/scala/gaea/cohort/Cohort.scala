5b4eb9f1ada626352b2b9a6a5e540043a09aca8a
Ryan Spangler <ryan.spangler@gmail.com>
2016-10-31 16:58:33 -0700

    generating matrix from sample variants

---
+++ graph/src/main/scala/gaea/cohort/Cohort.scala
@@ -0,0 +1,56 @@
+package gaea.cohort
+
+import gaea.graph._
+import gremlin.scala._
+import gaea.frame.Frame
+import gaea.collection.Collection._
+import org.apache.tinkerpop.gremlin.process.traversal.P._
+
+object Cohort {
+  val sampleStep = StepLabel[Vertex]()
+  val geneStep = StepLabel[String]()
+
+  class VariantBuilder(header: List[String], data: List[List[String]]) {
+    def addSample(sample: String) (variants: List[String]): VariantBuilder = {
+      val variantSet = variants.toSet
+      val newKeys = variantSet.diff(header.toSet).toList
+      val newHeader = header ++ newKeys
+      val row = sample :: newHeader.map(gene => if (variantSet(gene)) "1" else "0")
+      val newData = row :: data
+      new VariantBuilder(newHeader, newData)
+    }
+
+    def finish(): Seq[Seq[String]] = {
+      val lines = ("gene" :: header) :: data
+      val headerSize = lines.head.size
+      val output = lines.map { line =>
+        line ++ List.fill(headerSize - line.size) ("0")
+      }
+
+      transpose(output)
+    }
+  }
+
+  def transpose[A](matrix: Seq[Seq[A]]): Seq[Seq[A]] = {
+    val base = List.fill(matrix.head.size) (Vector[A]())
+    matrix.foldLeft(base) { (t, row) =>
+      t.zip(row).map { case (column, cell) =>
+        column :+ cell
+      }
+    }
+  }
+
+  def cohortVariants(graph: GaeaGraph) (cohort: String): Seq[Seq[String]] = {
+    val variants = graph.V.hasLabel("cohort").has(Gid, "cohort:" + cohort).out("hasMember").as(sampleStep).in("tumorSample").in("effectOf").out("inGene").value[String]("symbol").as(geneStep).select((sampleStep, geneStep)).toList
+    val samples = groupAs(variants) (_._1.property("gid").orElse("")) (_._2)
+    val base = new VariantBuilder(List[String](), List[List[String]]())
+    samples.foldLeft(base) { case (builder, (sample, genes)) =>
+      builder.addSample(sample) (genes)
+    }.finish()
+  }
+
+  def cohortVariantsTSV(graph: GaeaGraph) (cohort: String): String = {
+    val variants = cohortVariants(graph) (cohort)
+    Frame.renderTSV(variants)
+  }
+}
