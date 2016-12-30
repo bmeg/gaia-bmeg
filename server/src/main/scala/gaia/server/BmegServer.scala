c71e5b411ea39ec5da5f85e9b5f4f98c122fa228
Ryan Spangler <ryan.spangler@gmail.com>
2016-12-29 16:21:34 -0800

    server automatically loading configured facets on startup

--- server/src/main/scala/gaia/server/BmegServer.scala
+++ server/src/main/scala/gaia/server/BmegServer.scala
@@ -5,12 +5,14 @@ import gaia.graph._
 import gaia.facet._
 
 object BmegServer extends App {
-  val config = GaiaConfig.readConfig("resources/config/gaia.yaml")
-  val graph = config.connectToGraph(config.graph)
+  GaiaServer.startServer("resources/config/gaia.yaml")
 
-  if (graph.isSuccess) {
-    GaiaServer.start(config.server) (graph.get) (BmegFacets.facets)
-  } else {
-    println("failed to connect to graph: " + config.graph.toString)
-  }
+  // val config = GaiaConfig.readConfig("resources/config/gaia.yaml")
+  // val graph = config.connectToGraph(config.graph)
+
+  // if (graph.isSuccess) {
+  //   GaiaServer.start(config.server) (graph.get) (BmegFacets.facets)
+  // } else {
+  //   println("failed to connect to graph: " + config.graph.toString)
+  // }
 }
