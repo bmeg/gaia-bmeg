c2a95223dbf1d7f3d1acd4600072ac9993664ecf
Ryan Spangler <ryan.spangler@gmail.com>
2017-01-06 17:16:42 -0800

    update of main site

--- resources/public/static/vertex/js/vertex.jsx
+++ resources/public/static/vertex/js/vertex.jsx
@@ -2,175 +2,281 @@ var snipPrefix = function(s) {
   return s.substring(s.indexOf(':') + 1);
 }
 
-var exploreVertex = function(page, gid) {
-  var url = "/gaia/vertex/find/" + gid;
-  window.location.hash = gid
-
-  $.ajax({
-    url: url,
-    dataType: 'json',
-    type: 'GET',
-    success: function(result) {
-      if (Object.keys(result).length > 0) {
-        page.setState({vertex: result, lastMatch: page.state.input})
-      }
-    }.bind(page),
-
-    error: function(xhr, status, err) {
-      console.error(url, status, err.toString());
-    }.bind(page)
-  });
-}
-
-var VertexProperty = React.createClass({
-  getInitialState: function() {
-    return {};
-  },
-
-  render: function() {
-    return (
-      <div>
-        {this.props.name} : {this.props.property}
-      </div>
-    );
-  }
-});
-
-var VertexEdge = React.createClass({
-  getInitialState: function() {
-    return {};
-  },
-
-  render: function() {
-    return (
-      <li onClick={this.props.navigate(this.props.gid)}>{snipPrefix(this.props.gid)}</li>
-    )
-  }
-});
-
 var VertexEdges = React.createClass({
   getInitialState: function() {
     return {};
   },
 
   render: function() {
-    var edges = this;
-    var edgeList = this.props.edges.map(function(edge) {
-      return <VertexEdge key={edge} gid={edge} navigate={edges.props.navigate} />
-    })
+    var props = this.props;
+    var prefix = props.edges[0].split(':')[0]
+    var header = props.label + " (" + props.direction + " " + prefix + ")";
 
-    var prefix = this.props.edges[0].split(':')[0]
+    var items = props.edges.map(gid => (
+      <ExpandoItem key={gid}>
+        <a onClick={() => props.navigate(gid)}>{snipPrefix(gid)}</a>
+      </ExpandoItem>
+    ));
 
-    return (
-      <div>
-        <h4>{this.props.label} ({this.props.direction} {prefix})</h4>
-        <ul>
-          {edgeList}
-        </ul>
-      </div>
-    );
+    return <Expando header={header}>{items}</Expando>;
   }
 });
 
-var AlternateView = React.createClass({
-  getInitialState: function() {
-    return {}
-  },
+function PropertyRow(props) {
+  return (<tr>
+    <td className="prop-key mdl-data-table__cell--non-numeric">{props.name}</td>
+    <td className="mdl-data-table__cell--non-numeric">{props.value}</td>
+  </tr>)
+}
 
-  render: function() {
-    var page = this;
-    return (
-      <div onClick={this.props.navigate(this.props.back)}>{this.props.vertex.properties.gid}</div>
-    )
-  }
-});
+var PropertiesView = function(props) {
+  var properties = Object.keys(props.vertex.properties).map(function(key) {
+    var v = props.vertex.properties[key];
+    return <PropertyRow key={key} name={key} value={v} />
+  });
 
-var VertexView = React.createClass({
-  getInitialState: function() {
-    return {}
-  },
-
-  render: function() {
-    var page = this;
-    var properties = Object.keys(this.props.vertex.properties).map(function(key) {
-      var property = page.props.vertex.properties[key];
-      return <VertexProperty key={key} name={key} property={property} />
-    });
-
-    var inEdges = Object.keys(this.props.vertex['in']).map(function(key) {
-      return <VertexEdges key={key} label={key} navigate={page.props.navigate} edges={page.props.vertex['in'][key]} direction="from"/>
-    });
-
-    var outEdges = Object.keys(this.props.vertex['out']).map(function(key) {
-      return <VertexEdges key={key} label={key} navigate={page.props.navigate} edges={page.props.vertex['out'][key]} direction="to"/>
-    });
-
-    return (
-      <div>
-        <h2>Showing {this.props.vertex.type} vertex {snipPrefix(this.props.vertex.properties.gid)}</h2>
-        <div className="vertex-properties">
+  return (
+    <div>
+      <div className="vertex-properties">
+        <table
+          className="prop-table mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shad--2dp"
+        ><tbody>
           {properties}
-        </div>
-        <div className="vertex-in-edges">
-          <h3>In Edges</h3>
+        </tbody></table>
+      </div>
+    </div>
+  )
+}
+
+var EdgesView = function(props) {
+  console.log(props)
+  var inEdges = Object.keys(props.vertex['in'])
+  // Filter out edges with "hasInstance" in label
+  .filter(key => key != 'hasInstance')
+  .map(function(key) {
+    return <VertexEdges
+      key={key}
+      label={key}
+      navigate={props.navigate}
+      edges={props.vertex['in'][key]}
+      direction="from"
+    />
+  });
+   var outEdges = Object.keys(props.vertex['out'])
+  // Filter out edges with "hasInstance" in label
+  .filter(key => key != 'hasInstance')
+  .map(function(key) {
+    return <VertexEdges
+      key={key}
+      label={key}
+      navigate={props.navigate}
+      edges={props.vertex['out'][key]}
+      direction="to"
+    />
+  });
+
+  return (
+    <div>
+      <div className="vertex-edges-wrapper">
+        <div className="vertex-in-edges vertex-edges">
+          <h4>In Edges</h4>
           {inEdges}
         </div>
-        <div className="vertex-out-edges">
-          <h3>Out Edges</h3>
+        <div className="vertex-out-edges vertex-edges">
+          <h4>Out Edges</h4>
           {outEdges}
         </div>
       </div>
-    )
-  }
-});
+    </div>
+  )
+}
 
 var VertexInput = React.createClass({
-  getInitialState: function() {
-    var hash = window.location.hash.substr(1);
-    exploreVertex(this, hash);
+  componentDidMount() {
+    componentHandler.upgradeElement(this.refs.mdlWrapper)
+  },
+  render() {
+    return <div
+      className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label"
+      ref="mdlWrapper"
+    >
+      <label
+        className="mdl-textfield__label"
+        htmlFor="vertex-gid-input"
+      >Enter a vertex GID</label>
+      <input
+        id="vertex-gid-input"
+        type="text"
+        name="gid"
+        className="mdl-textfield__input"
+        onChange={e => this.props.onChange(e.target.value)}
+        value={this.props.value}
+      />
+    </div>
+  },
+})
 
+
+var Expando = React.createClass({
+  getInitialState() {
     return {
-      input: hash,
-      lastMatch: hash,
-      back: '',
-      vertex: {}
+      collapsed: true,
+    }
+  },
+  componentDidMount() {
+    var content = $(this.refs.content)
+    content.css('margin-top', -content.height());
+  },
+  onClick() {
+    this.setState({collapsed: !this.state.collapsed})
+  },
+  render() {
+    var props = this.props;
+    var rootClassName = classNames("expando", "mdl-collapse", "mdl-navigation", {
+      "mdl-collapse--opened": !this.state.collapsed,
+    })
+    
+    return (<div className={rootClassName}>
+      <a className="mdl-navigation__link mdl-collapse__button expando-header" onClick={this.onClick}>
+        <i className="material-icons mdl-collapse__icon mdl-animation--default">expand_more</i>
+        {props.header}
+      </a>
+      <div className="mdl-collapse__content-wrapper expando-content">
+        <div className="mdl-collapse__content mdl-animation--default" ref="content">
+          {props.children}
+        </div>
+      </div>
+    </div>)
+  }
+})
+
+function ExpandoItem(props) {
+  return <span className="mdl-navigation__link">{props.children}</span>
+}
+
+var PubmedLink = function(props) {
+  var url = "https://www.ncbi.nlm.nih.gov/pubmed/" + props.id;
+  console.log(url);
+  return (<div><a href={url} target="_blank">{url}</a></div>)
+}
+
+var VertexViewer = React.createClass({
+  getInitialState() {
+    return {
+      input: this.getGIDFromURL(),
+      loading: false,
+      error: "",
+      vertex: {},
     };
   },
 
-  changeInput: function(event) {
-    var gid = event.target.value;
-    this.setState({input: gid, back: this.state.input});
-    exploreVertex(this, gid);
+  getGIDFromURL() {
+    return getParameterByName("gid")
+  },
+
+  componentDidMount() {
+    window.onpopstate = this.onPopState
+    if (this.state.input) {
+      this.setVertex(this.state.input, true)
+    }
+  },
+
+  onPopState(e) {
+    var hash = this.getGIDFromURL();
+    if (e.state && e.state.gid) {
+      this.setVertex(e.state.gid, true)
+    } else if (hash) {
+      this.setVertex(hash, true)
+    } else {
+      this.setVertex()
+    }
+  },
+
+  setVertex(gid, nopushstate) {
+    if (!gid) {
+      this.setState({vertex: {}, error: ""})
+    } else {
+      var url = "/gaia/vertex/find/" + gid;
+      this.setState({input: gid, loading: true, error: ""});
+      $.ajax({
+        url: url,
+        dataType: 'json',
+        type: 'GET',
+        success: result => {
+          if (Object.keys(result).length > 0) {
+            this.setState({vertex: result, loading: false, error: ""})
+            if (!nopushstate) {
+              // Only push state to history if we found an actual vertex
+              // This avoids pushing state for intermediate queries.
+              history.pushState({gid: gid}, "Vertex: " + gid, '?gid=' + gid);
+            }
+          } else {
+            this.setState({vertex: {}, loading: false, error: ""})
+          }
+        },
+        error: (xhr, status, err) => {
+          this.setState({loading: false, error: err.toString()})
+          console.error(url, status, err.toString())
+        },
+        timeout: 3000,
+      });
+    }
   },
 
   render: function() {
-    var page = this;
-    var navigate = function(gid) {
-      return function() {
-        page.setState({input: gid, back: page.state.input, lastMatch: gid});
-        exploreVertex(page, gid);
-      }
-    };
+    var loading = "";
+    if (this.state.loading) {
+      loading = <div className="mdl-spinner mdl-js-spinner is-active"></div>
+    }
 
-    var vertex = <div className="empty-vertex"></div>;
-    if (this.state.vertex['properties']) {
-      if (this.state.vertex.properties.type === 'Individual') {
-        vertex = <VertexView navigate={navigate} vertex={this.state.vertex} back={this.state.back}/>
-      } else {
-        vertex = <VertexView navigate={navigate} vertex={this.state.vertex} />
+    var error;
+    if (this.state.error) {
+      error = <div>Request error: {this.state.error}</div>
+    }
+
+    var emptyMessage = "";
+    if (this.state.input) {
+      emptyMessage = "No vertex found";
+    }
+
+    var vertex = <div className="empty-vertex">{emptyMessage}</div>;
+    var visualizations = [];
+
+    // The vertex isn't empty, so create a VertexView
+    if (this.state.vertex.properties) {
+      vertex = (<div><PropertiesView vertex={this.state.vertex} /><EdgesView vertex={this.state.vertex} navigate={this.setVertex} /></div>)
+
+      if (this.state.vertex.properties.type === 'Pubmed') {
+        console.log(this.state.vertex.properties);
+        var link = (<PubmedLink key="pubmed-link" id={this.state.vertex.properties.pmid} />)
+        visualizations.push(link);
       }
     }
 
     return (
       <div>
-        <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-          <label className="mdl-textfield__label" htmlFor="vertex-gid-input">Enter a vertex GID</label>
-          <input id="vertex-gid-input" type="text" name="gid" className="mdl-textfield__input" onChange={(e) => this.changeInput(e)} value={this.state.input} />
-        </div>
+        <VertexInput onChange={this.setVertex} value={this.state.input} />
+        {loading}
+        {visualizations}
+        {error}
         {vertex}
       </div>
     );
   }
 });
 
-ReactDOM.render(<VertexInput />, document.getElementById('vertex-explore'));
+window.addEventListener('load', function() {
+  ReactDOM.render(<VertexViewer />, document.getElementById('vertex-explore'));
+})
+
+function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
